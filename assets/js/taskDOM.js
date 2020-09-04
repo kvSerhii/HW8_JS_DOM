151ad3c63a1f370cbfba23e33a0aa32a77de6732
kvSerhii <kv.Serhii@gmail.com>
2020-09-04 10:18:19 +0300

    Home Work 8: JS DOM
    add task of changein button name by clicking it (<input type='button'>)
    
    files and directories:
    	- index.html (changed)
    	- assets/js/taskDOM.js (changed)

--- assets/js/taskDOM.js
+++ assets/js/taskDOM.js
@@ -9,3 +9,12 @@ btn1.addEventListener('click', btnHandler);
 function btnHandler(event) {
     alert(inp.value);
 }
+
+// 2) По нажатию на кнопку изменить текст самой  кнопки
+const btn2 = document.getElementById('btn2');
+
+btn2.addEventListener('click', changeButtonName);
+
+function changeButtonName(event) {
+    btn2.value = 'Already pressed!';
+}
